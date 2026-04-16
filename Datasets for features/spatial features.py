1a322d7bbbda38473d8f6ff26f5d0d933e192bbe
anyakudojunior <anyakudojunior@gmail.com>
2026-04-16 08:34:57 +0200

    spatial features.py

---
+++ Datasets for features/spatial features.py
@@ -0,0 +1,7 @@
+sites = pd.read_csv("sites.csv")
+
+# storing what we need in a new dataframe to later merge with the hourly data
+sites = sites[["counting_station", "latitude", "longitude", "municipality"]]
+
+# merge with data
+df = df.merge(sites, on"counting_station", how="left")
