3d718be80787a1e0dee6222b09875d599143d52f
DayaLuna <94837691+DayaLuna@users.noreply.github.com>
2026-05-07 14:08:29 +0200

    Deleting things that are not necessary any more

--- Datasets for features/archive/spatial features.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-sites = pd.read_csv("sites.csv")
-
-# storing what we need in a new dataframe to later merge with the hourly data
-sites = sites[["counting_station", "latitude", "longitude", "municipality"]]
-
-# merge with data
-finaldataset = data.merge(sites, on"counting_station", how="left")
